eb061e5e91fa6f6571ff37a6debfb9e332f563c3
Lenovo G41 <Lenovo G41@JessJess>
2020-07-01 04:11:02 -0500

    comentar y descomentar la parte de dbdebug para que en produccion no aprescan los errores de la bd

--- application/config/database.php
+++ application/config/database.php
@@ -84,6 +84,7 @@ $db['default'] = array(
 	'dbprefix' => '',   
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
+//	'db_debug' => FALSE, //subir al servidor
 	'cache_on' => FALSE,
 	'cachedir' => '',
 	'char_set' => 'utf8',
